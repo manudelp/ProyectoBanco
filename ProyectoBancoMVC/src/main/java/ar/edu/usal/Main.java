0382660a44ad526422cbad3aec2f4cddfcfdf619
Manuel Delpino <manudelpino0@gmail.com>
2025-06-10 11:39:24 -0300

    Refactor main method and update account data

--- ProyectoBancoMVC/src/main/java/ar/edu/usal/Main.java
+++ ProyectoBancoMVC/src/main/java/ar/edu/usal/Main.java
@@ -2,12 +2,8 @@ package ar.edu.usal;
 
 import ar.edu.usal.vista.VentanaPrincipal;
 
-import javax.swing.SwingUtilities;
-
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new VentanaPrincipal().setVisible(true);
-        });
+        new VentanaPrincipal().setVisible(true);
     }
 }
